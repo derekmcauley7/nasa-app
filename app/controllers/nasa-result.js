9c34198dbf2a1fc7084686462dcaa681afd41014
Derek McAuley <derekmcauley7@gmail.com>
2018-06-23 16:45:16 +0100

    started adding a text image button and small fixes

--- app/controllers/nasa-result.js
+++ app/controllers/nasa-result.js
@@ -1,12 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  number: 3,
-  title : 'model.collection.items.1.data.0.title',
-  link: 'model.collection.items.0.links.0.href',
   actions: {
-    next: function(){
-      console.log(this.title);
+    loadNext: function(){
+      var num = 2;
+      console.log(num);
+      this.transitionToRoute("nasa-result");
     }
   }
 
