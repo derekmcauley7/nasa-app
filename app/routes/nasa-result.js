8a7d8daede85b2b9c0118b83e1fc7eea001fd802
Derek McAuley <derekmcauley7@gmail.com>
2018-07-23 18:00:08 +0100

    Started using promises for results and fixed tests

--- app/routes/nasa-result.js
+++ app/routes/nasa-result.js
@@ -1,11 +1,33 @@
 import Route from '@ember/routing/route';
-import $ from 'jquery';
+
+function getJSON(url) {
+  return new Promise(function(resolve, reject){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = handler;
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send();
+    function handler() {
+      if (this.readyState === this.DONE) {
+        if (this.status === 200) {
+          resolve(this.response);
+        } else {
+          reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+        }
+      }
+    }
+  });
+}
 
 export default Route.extend({
-
   model: function() {
     var url =  "https://images-api.nasa.gov/search?q=" + sessionStorage.getItem("SessionName") + "&media_type=image";
-    return $.getJSON(url);
+    return getJSON(url).then(function(json) {
+      return json;
+    }, function(reason) {
+      console.log("Error Message: " + reason);
+    });
 },
 
 actions: {
--- app/routes/nasa-result3.js
+++ app/routes/nasa-result3.js
@@ -1,10 +1,33 @@
 import Route from '@ember/routing/route';
-import $ from 'jquery';
-export default Route.extend({
+function getJSON(url) {
+  return new Promise(function(resolve, reject){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = handler;
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send();
 
-  model: function() {
-    var url =  "https://images-api.nasa.gov/search?q=" + sessionStorage.getItem("SessionName") + "&media_type=image";
-    return $.getJSON(url);
+    function handler() {
+      if (this.readyState === this.DONE) {
+        if (this.status === 200) {
+          resolve(this.response);
+        } else {
+          reject(new Error('getJSON: `' + url + '` failed with status: [' + this.status + ']'));
+        }
+      }
+    }
+  });
+}
+
+export default Route.extend({
+model: function() {
+  var url =  "https://images-api.nasa.gov/search?q=" + sessionStorage.getItem("SessionName") + "&media_type=image";
+  return getJSON(url).then(function(json) {
+    return json;
+  }, function(reason) {
+    console.log("Error Message: " + reason)
+  });
 },
 
 actions: {
